de96cce54f2567048dab9652d4bf3dd527a46a38
Karthik Nishanth <nishanthkarthik@live.com>
2017-12-25 22:40:20 +0530

    Fix incorrect xml
    The xml spec states that no whitespace must exist before the first `xml` tag

--- llvm-to-dash.py
+++ llvm-to-dash.py
@@ -115,7 +115,7 @@ def add_urls(db, cur):
 
 def add_infoplist():
   name = docset_name.split('.')[0]
-  info = ' <?xml version=\'1.0\' encoding=\'UTF-8\'?>' \
+  info = '<?xml version=\'1.0\' encoding=\'UTF-8\'?>' \
           '<!DOCTYPE plist PUBLIC \'-//Apple//DTD PLIST 1.0//EN\' \'http://www.apple.com/DTDs/PropertyList-1.0.dtd\'> ' \
           '<plist version=\'1.0\'> ' \
           '<dict> ' \
